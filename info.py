1befae31203a0e1520fa725f71f91ecec879aaa4
Ravi7019 <156110174+Ravi7019@users.noreply.github.com>
2024-05-04 08:55:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,8 +29,8 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8
 CODE = (environ.get('CODE', 'https://telegra.ph/file/72f425007b22d28bd935e.jpg'))
 
 #stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', 'linkshortify.com'))
-STREAM_API = (environ.get('STREAM_API', 'f633a78dbd69f2b98e010471451fa0244d58b1e6'))
+STREAM_SITE = (environ.get('STREAM_SITE', 'shorito.com'))
+STREAM_API = (environ.get('STREAM_API', '05f86d3b418ec84c9c51a7175ce1d43db550c88b'))
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/How_To_Open_Links_bypass'))
 
 # Admins, Channels & Users
@@ -62,8 +62,8 @@ VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( F
 HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/How_To_Open_Links_bypass') # How to open tutorial link for verification
 
 # Others
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkshortify.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'f633a78dbd69f2b98e010471451fa0244d58b1e6')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shorito.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '05f86d3b418ec84c9c51a7175ce1d43db550c88b')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))  # else--> True
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
